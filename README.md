feb23d2df88e78114929d49c90f38e07db3d6c03
Collin <ccr243@nau.edu>
2022-10-06 22:22:45 -0700

    Added 3 more functions to v3test and updated readme
    Collin Rampata updated v3test.c and README.md

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ Collin Rampata: ccr243@nau.edu
 
 # Usage
 
+After making, run using: "./v3test"
+
 # Known Issues
+
+Strictly computes v3 vectors and does not work for vectors of size != v3
--- v3math.c
+++ v3math.c
@@ -2,14 +2,21 @@
 #include "v3math.h"
 
 // Function Name: v3_from_points
-// Function Purpose: 
+// Function Purpose: Calculate v3 vector from two 3 dimensional vectors
 void v3_from_points(float *dst, float *a, float *b)
 {
+
+	int index = 0;
+
+	for(index = 0; index < 3; index++)
+	{
+		dst[index] = b[index] - a[index];
+	}	
 
 }
 
 // Function Name: v3_add
-// Function Purpose: Give some
+// Function Purpose: Give sum
 void v3_add(float *dst, float *a, float *b)
 {
 	int index = 0;
@@ -22,7 +29,7 @@ void v3_add(float *dst, float *a, float *b)
 }
 
 // Function Name: v3_subtract
-// Function Purpose: Take some
+// Function Purpose: Take difference
 void v3_subtract(float *dst, float *a, float *b)
 {
 
@@ -36,7 +43,7 @@ void v3_subtract(float *dst, float *a, float *b)
 }
 
 // Function Name: v3_dot_product
-// Function Purpose: 
+// Function Purpose: Calculate dot product
 float v3_dot_product(float *a, float *b)
 {
 	int index = 0;
@@ -52,15 +59,20 @@ float v3_dot_product(float *a, float *b)
 }
 
 // Function Name: v3_cross_product
-// Function Purpose: 
+// Function Purpose: Calculate cross product
 void v3_cross_product(float *dst, float *a, float *b)
 {
+	// It is assumed that we will strictly be dealing with v3 vectors 
+	// so indicies are reached using hard-coded integers
+	dst[0] = (a[1] * b[2]) - (a[2] * b[1]);
 
+	dst[1] = (a[2] * b[0]) - (a[0] * b[2]);
 
+	dst[2] = (a[0] * b[1]) - (a[1] * b[0]);
 }
 
 // Function Name: v3_scale
-// Function Purpose: 
+// Function Purpose: Scales an input vector by s 
 void v3_scale(float *dst, float s)
 {
 	int index = 0;
@@ -76,7 +88,9 @@ void v3_scale(float *dst, float s)
 // Function Purpose: 
 float v3_angle(float *a, float *b)
 {
-    return 0.0; //stub
+	float angleBetween = 0.0;
+
+    return angleBetween; //stub
 }
 
 // Function Name: v3_angle_quick
@@ -97,7 +111,19 @@ void v3_reflect(float *dst, float *v, float *n)
 // Function Purpose:
 float v3_length(float *a)
 {
-    return 0.0; //stub
+	float index0Power = 0.0;
+
+	float index1Power = 0.0;
+
+	float index2Power = 0.0;
+
+	index0Power = a[0] * a[0];
+
+	index1Power = a[1] * a[1];
+
+	index2Power = a[2] * a[2];
+
+    return fabs(sqrt( index0Power + index1Power + index2Power )); //stub
 }
 
 // Function Name: v3_normalize
